f97e896fd87fbea9a0f185df4fc33c54429d666a
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-02-18 05:46:21 +0000

    #

---
+++ q1.ts
@@ -0,0 +1,26 @@
+const heros = ["herh","bgh"]
+
+heros.map((hero): string =>{
+    return hero 
+})
+
+//return object
+function createuser ():{name:string , price : number}{
+    return {name:"saku", price:68}
+}
+
+// type User ={
+//     name:string,
+//     email: string,
+//     isactive :boolean
+// }
+
+// function createUser (user:User): User{
+//     return {name:"ueyfcjgj",email:"jfbcjs", isactive:true}
+// }
+
+type User ={
+    name:string,
+    email: string,
+    isactive :boolean
+}
